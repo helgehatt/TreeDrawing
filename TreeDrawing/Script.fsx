0bbee20fcbbfb9efc618bc14bc5bbffbf44cbd6a
Helge <helge@hatteland.com>
2017-01-13 13:31:51 +0100

    Added PS functions

--- TreeDrawing/Script.fsx
+++ TreeDrawing/Script.fsx
@@ -57,4 +57,53 @@ let createTree breadth depth = let subtrees = createSubTree breadth (depth - 1)
                                Node(("root",0.0),subtrees)
 
 
+let nl = System.Environment.NewLine
+let FloatMax = System.Double.MaxValue
+let FloatMin = System.Double.MinValue
+let NodeToLine = -20.0
+let LineToNode = -40.0
 
+let drawNode (x,y) (Node((label,_),subtrees)) =
+    string (x,y-10.0) + " moveto" + nl +
+    string (label) + " dup stringwidth pop 2 div neg 0 rmoveto show" + nl +
+    string (x,y-20.0) + " moveto" + nl,
+    (x,y-20.0)
+    
+let drawHorizontal (x,y) subtrees =
+    let rec outliers mini maxi = function
+        | [] ->  (mini, maxi)
+        | Node((_, x), _)::es -> outliers (min x mini) (max x maxi) es
+    let (min,max) = outliers FloatMax FloatMin subtrees    
+    string(min,y) + " moveto" + nl + 
+    string(max,y) + " lineto" + nl
+
+let drawVerticalFromNode (x,y) =
+    string(x,y+NodeToLine) + " lineto" + nl, 
+    (x,y+NodeToLine)
+
+let rec drawVerticalFromLine (x,y) = function
+    | [] -> ""
+    | Node((_,d),_)::es -> string(x+d,y) + " moveto" + nl + 
+                           string(x+d,y+LineToNode) + " lineto" + nl +
+                           drawVerticalFromLine (x,y) es
+                           
+
+let rec drawTree (x,y) (Node((label,d),subtrees)) =
+    let (nodeStr, nodePt) = drawNode (x,y) (Node((label,d),subtrees))
+    match subtrees with
+    | []    -> nodeStr
+    | _     -> let (fromNodeStr, fromNodePt) = drawVerticalFromNode nodePt
+               let horizontalLine = drawHorizontal fromNodePt subtrees
+               let fromLineStr = drawVerticalFromLine (fromNodePt) subtrees
+               String.concat (nodeStr + fromNodeStr + horizontalLine + fromLineStr + "stroke" + nl)
+                    (List.map (drawTree fromNodePt) subtrees)
+
+let createPS tree =
+   "%!" + nl +
+    "<</PageSize[1400 1000]/ImagingBBox null>> setpagedevice" + nl +
+    "1 1 scale" + nl +
+    "700 999 translate" + nl +
+    "newpath" + nl +
+    "/Times-Roman findfont 10 scalefont setfont" + nl +
+    drawTree (0.0, 0.0) tree + 
+    "showpage"
