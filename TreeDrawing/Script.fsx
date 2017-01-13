bb79f8a59abbe5009f37744c3c9ea60a2518b48b
Helge <helge@hatteland.com>
2017-01-13 12:04:52 +0100

    Fixed positioning

--- TreeDrawing/Script.fsx
+++ TreeDrawing/Script.fsx
@@ -12,30 +12,30 @@ let rec merge = function
 
 let mergelist es = List.fold (fun acc elem -> merge (acc,elem)) [] es
 
-let rec fit ps qs =
-    match (ps,qs) with 
-    | ((_,p)::ps', (q,_)::qs') -> max (fit ps qs) (p - q + 1.0)
+let rec fit left right =
+    match (left, right) with 
+    | ((_,p)::ps, (q,_)::qs) -> max (fit ps qs) (p - q + 1.0)
     | _                        -> 0.0
 
 let fitlistl es =
-    let rec fitlistl' acc = function
+    let rec aux acc = function
         | [] -> []
         | e::es -> let x = fit acc e
-                   x :: fitlistl' (merge(acc, moveextent (e,x))) es
-    fitlistl' [] es
+                   x :: aux (merge(acc, moveextent (e,x))) es
+    aux [] es
 
 let fitlistr es = 
-    let rec fitlistr' acc = function
+    let rec aux acc = function
         | [] -> []
-        | e::es -> let x = fit e acc
-                   x :: fitlistr' (merge(moveextent (e,x), acc)) es
-    fitlistr' [] (List.rev es)
+        | e::es -> let x = -(fit e acc)
+                   x :: aux (merge(moveextent (e,x), acc)) es
+    List.rev (aux [] (List.rev es))
 
 let mean (x,y) = (x + y) / 2.0
 
 let fitlist es = List.map mean (List.zip (fitlistl es) (fitlistr es))
 
-let rec design' (Node((label,x), subtrees)) =
+let rec design' (Node((label,_), subtrees)) =
     let (trees, extents) = List.unzip (List.map design' subtrees)
     let positions = fitlist extents
     let ptrees = List.map movetree (List.zip trees positions)
